e060cffef2661e406a1c538b38a6daa4d25a60b3
MD-IRSHAD <114646710+MD-IRSHAD@users.noreply.github.com>
2023-08-12 22:59:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function decimalToBinary(num) {
   //Write you code here
+	  if (num === 0) {
+        return '0';
+    }
+
+    let binary = '';
+    while (num > 0) {
+        binary = (num % 2) + binary;
+        num = Math.floor(num / 2);
+    }
+
+    return binary;
+}
+
+window.decimalToBinary = decimalToBinary;
+You can use this code in a web environment where you have access to the window object. The decimalToBinary function takes a decimal number as input and returns its binary representation. You can call this function using window.decimalToBinary(number) and pass the decimal number you want to convert.
+
+
+
+
+
+
   
 }
 
