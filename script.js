81a7ef573ece328c736b32537fd78531bde77e2a
MD-IRSHAD <114646710+MD-IRSHAD@users.noreply.github.com>
2023-08-17 18:35:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function decimalToBinary(num) {
-  //Write you code here
-	  if (num === 0) {
+function decimalToBinary(decimalNumber) {
+    if (decimalNumber === 0) {
         return '0';
     }
 
     let binary = '';
-    while (num > 0) {
-        binary = (num % 2) + binary;
-        num = Math.floor(num / 2);
+    while (decimalNumber > 0) {
+        binary = (decimalNumber % 2) + binary;
+        decimalNumber = Math.floor(decimalNumber / 2);
     }
 
     return binary;
 }
 
-}
-
-window.decimalToBinary = decimalToBinary;
+// Test cases
+console.log(decimalToBinary(7));   // Output: "111"
+console.log(decimalToBinary(10));  // Output: "1010"
+console.log(decimalToBinary(33));  // Output: "100001"
